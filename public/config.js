6b77bb72b2f2ac9a9d8c9a9fe852d57350e77e12
cvsschaitanya <cvsschaitanya@gmail.com>
2025-02-27 03:29:07 +0530

    CLEAN BRANCH

--- public/config.js
+++
@@ -1,5 +0,0 @@
-const media_remote_config = {
-    server: "192.168.0.103:9000",
-};
-
-media_remote_config;
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-// import { useEffect, useState } from 'react'
-// import axios from 'axios'
-import './App.css'
-import Home from './components/Home'
-
-export default function App() {
-    // eslint-disable-next-line no-undef
-    return <Home config={media_remote_config} />
-};
